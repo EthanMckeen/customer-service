1f23194c31c2d9b7809f6cb6a7720bc49c663753
EthanMckeen <101148661+EthanMckeen@users.noreply.github.com>
2024-08-09 18:40:47 -0700

    The text box isnt displaying messages @~16:22 in vid

--- app/page.js
+++ app/page.js
@@ -1,18 +1,59 @@
 'use client'
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
 
 
 export default function Home() {
- const [messages, setMessages] = useState({
+ const [messages, setMessages] = useState([{
   role: 'assistant',
   content: 'Hi how can I help?'
- })
+ }])
 
- const [msg, setMsg] = useState('')
+ const [message, setMessage] = useState('')
 
  return(
-  <Box/>
+    <Box 
+        width='100vw' 
+        height='100vh'
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'>
+            <Stack
+                direction='column'
+                width='600px'
+                height='700px'
+                border='2px solid black'
+                p={2}
+                spacing={2}>
+                    <Stack
+                        direction='column'
+                        spacing={2}
+                        flexGrow={1}
+                        overflow='auto'
+                        maxHeight='100%'>
+                            {messages.map((message, index)=>{
+                                <Box
+                                    key ={index}
+                                    display= 'flex'
+                                    justifyContent={messages.role=== 'assistant' ? 'flex-start' : 'flex-end' } >
+                                        <Box
+                                        bgcolor={
+                                            messages.role === 'assistant'
+                                            ? 'primary.main'
+                                            : 'secondary.main'
+                                        }
+                                        color='red'
+                                        borderRadius={16}
+                                        p={3}>
+                                            {messages.content}
+                                        </Box>
+                                    </Box>
+                                })
+                            }
+                    </Stack>
+            </Stack>
+    </Box>
  )
 }
